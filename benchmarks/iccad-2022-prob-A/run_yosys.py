84eeb96f0c07e09b3d2c7ff3eb30ad1ac5c4c818
Brooks LJ <jiang.long@gmail.com>
2023-06-25 14:28:14 +0800

    ci

--- benchmarks/iccad-2022-prob-A/run_yosys.py
+++ benchmarks/iccad-2022-prob-A/run_yosys.py
@@ -5,12 +5,31 @@ import subprocess
 
 cmd = 'find . -name *.v'
 
-a = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
-ax = a.stdout.decode('utf-8').split()
+def run_cmd(cmd):
+    a = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+    ax = a.stdout.decode('utf-8')
+    return ax
 #print('\n'.join(ax))
 
+ax = run_cmd(cmd).split()
 for i in ax:
     e = 'yosys -p "read_verilog -sv %s ; hierarchy -auto-top ; flatten ; aigmap; write_aiger %s.aig"' % (i,i)
     print(e)
-    os.system(e)
+    #os.system(e)
     pass
+
+
+def sort_them_by_size():
+    cmd ='ls -lhS */*/*.aig'
+    
+    ax  = run_cmd(cmd).splitlines()
+    ax = [i.split()[-1] for i in ax]
+    ax = list(reversed(ax))
+    print('\n'.join(ax))
+    for i,j in enumerate(ax):
+        cmd = f"ln -sf ../{j} links/pa_{str(i).zfill(2)}.aig"
+        print(cmd)
+        os.system(cmd)
+    pass
+
+sort_them_by_size()
