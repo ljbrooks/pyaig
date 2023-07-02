76c41263945c51abff2b252f6d2b5adc59a6ad67
Brooks LJ <jiang.long@gmail.com>
2023-07-02 18:35:58 +0800

    ci

--- jsrc/scr/Term.py
+++ jsrc/scr/Term.py
@@ -5,7 +5,6 @@ sym = lambda t: t.symbol
 from scr.TermMgr import *    
 class Term:                     # base
 
-
     def __init__(self, **kwargs):
         self.nid=None
         self.termx=[]
@@ -17,20 +16,21 @@ class Term:                     # base
     def __repr__(self):
         return f'{self.__class__.__name__}: {str(self)}'
     def __or__(self, b):
-        return ExprOr(self,b)
+        return TermMgr.builder.__or__(self,b)
     def __and__(self,b):
-        return ExprAnd(self,b)
+        return TermMgr.builder.__and__(self,b)
     def __add__(self,b):
-        return ExprAdd(self,b)
+        return TermMgr.builder.__add__(self,b)
     def sigma(self, *termx):
-        return ExprSum(termx)
+        return TermMgr.builder.__sigma__(*termx)
     def __neg__(self):
-        return ExprNeg(self)
+        return TermMgr.builder.__neg__(self)
     def __invert__(self):
-        return ExprInv(self)
+        return TermMgr.builder.__invert__(self)
     def __hash__(self):
         return hash(self.uid)
-    
+    def re_eval(self):
+        return self    
     pass
 
 
@@ -40,6 +40,7 @@ class Atom(Term):
         pass
     def __str__(self):
         return self.nid
+
     pass
 
 class ConstOne(Term):
@@ -48,7 +49,6 @@ class ConstOne(Term):
         self.nid = '1'
         pass
     def __str__(self):
-
         return '1'
     pass
 
@@ -70,26 +70,36 @@ class ExprUnary(Term):
     pass
 class ExprSigma(Expr):
     OP='+'
+    def re_eval(self):
+        return reduce(lambda a,b: a+b, self.termx[1:], self.termx[0])
     pass
 
 class ExprXor(Expr):
     OP='^'
+    def re_eval(self):
+        return reduce(lambda a,b: a^b, self.termx[1:], self.termx[0])
     pass
 
 class ExprAnd(Expr):
     OP='&'
+    def re_eval(self):
+        return reduce(lambda a,b: a&b, self.termx[1:], self.termx[0])
     pass
-
+class ExprOr(Expr):
+    OP='|'
+    def re_eval(self):
+        return reduce(lambda a,b: a|b, self.termx[1:], self.termx[0])
+    pass
 class ExprNeg(ExprUnary):
     OP='-'
+    def re_eval(self):
+        return - self.termx[0]
     pass
 
 class ExprInv(ExprUnary):
     OP='~'
-    pass
-
-class ExprOr(Expr):
-    OP='|'
+    def re_eval(self):
+        return ~ self.termx[0]
     pass
 
 class Func (Expr):
@@ -102,14 +112,20 @@ class Func (Expr):
 
 class FuncS(Func):
     F='s'
+    def re_eval(self):
+        return TermMgr.builder.f(self.termx) #reduce(lambda a,b: a|b, self.termx[1:], self.termx[0])
     pass
 
 class FuncC(Func):
     F = 'c'
+    def re_eval(self):
+        return TermMgr.builder.c(self.termx) #reduce(lambda a,b: a|b, self.termx[1:], self.termx[0])
     pass
 
 class FuncD(Func):
     F = 'd'
+    def re_eval(self):
+        return TermMgr.builder.d(self.termx) #reduce(lambda a,b: a|b, self.termx[1:], self.termx[0])
     pass
 
 class scr:
@@ -117,6 +133,18 @@ class scr:
     c = FuncC
     d = FuncD
     pass
+
+def rewrite(tx, rewriter):
+    assert isinstance(t)
+    old_eval = TermMgr.builder
+    rewrite_r(tx)
+    TermMgr.builder = old_eval
+    pass
+
+def rewrite_r(tx):
+    if isinstance(tx, list):
+        return list(map(rewrite_r, tx))
+    return tx.re_eval()
 
 if __name__ == '__main__':
 
--- jsrc/scr/TermMgr.py
+++ jsrc/scr/TermMgr.py
@@ -1,7 +1,9 @@
 class TermMgr(list):
+    builder = None
     tmgr = None
     UID = 0
     pass
 
 if TermMgr.tmgr is None:
     TermMgr.tmgr = TermMgr()
+
