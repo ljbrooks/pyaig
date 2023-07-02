e69ac81602be946aa15ebe2998b1076009574ce9
Brooks LJ <jiang.long@gmail.com>
2023-07-02 22:25:48 +0800

    ci

--- jsrc/run.py
+++ jsrc/run.py
@@ -1,29 +1,32 @@
-from scr.TermTopo  import *
+from scr.TermTopo import *
 import pdb
-from scr.TermDot  import *
-from scr.TermMgr  import *
+from scr.TermDot import *
+from scr.TermMgr import *
 from scr.TermBuilder import *
 from scr.TermRewriter import *
 import math
 from tabulate import *
+
 TermMgr.builder = TermBuilder
 from s import *
 
-print('INFO: pox = ', [i.nid for i in pox])
+print("INFO: pox = ", [i.nid for i in pox])
 
 tm = TermMgr.tmgr
 
 tx = list(filter(lambda i: i.nid, tm))
 
 
-
 topo = TermTopo(pox)
 tx = list(filter(lambda i: i.d != math.inf, tm))
-def topoOrder(with_t = True):
-    for t in sorted(tx, key = lambda i: i.f):
-        print(t.color, t.d, t.f, t.nid, t.pi.nid, t.uid, '' if not with_t else t)
+
+
+def topoOrder(with_t=True):
+    for t in sorted(tx, key=lambda i: i.f):
+        print(t.color, t.d, t.f, t.nid, t.pi.nid, t.uid, "" if not with_t else t)
         pass
     pass
+
 
 print(tabulate([[i.nid, str(i)] for i in topo.topoOrder()]))
 
