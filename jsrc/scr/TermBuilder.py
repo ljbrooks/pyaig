33384be050bfaf09a473e0529f120fd1479ad2e4
Brooks LJ <jiang.long@gmail.com>
2023-07-02 18:36:10 +0800

    ci

---
+++ jsrc/scr/TermBuilder.py
@@ -0,0 +1,31 @@
+from scr.Term import *
+class TermBuilder:
+
+    @staticmethod
+    def __or__(a,b):
+        return ExprOr(self,b)
+    @staticmethod
+    def __and__(self,b):
+        return ExprAnd(self,b)
+    @staticmethod
+    def __add__(self,b):
+        return ExprAdd(self,b)
+    @staticmethod
+    def __sigma__(self, *termx):
+        return ExprSum(*termx)
+    @staticmethod
+    def __neg__(self):
+        return ExprNeg(self)
+    @staticmethod
+    def __invert__(self):
+        return ExprInv(self)
+    @staticmethod
+    def d(termx):
+        return FuncD(self)
+    @staticmethod
+    def s(termx):
+        return FuncS(self)
+    @staticmethod
+    def c(termx):
+        return FuncC(self)
+    pass
