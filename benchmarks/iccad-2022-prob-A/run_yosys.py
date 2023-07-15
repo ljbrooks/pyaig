183891f657c07180fb99b88c950cfea45f39a5f7
Brooks LJ <jiang.long@gmail.com>
2023-07-15 22:10:00 +0800

    CI: nothing major

--- benchmarks/iccad-2022-prob-A/run_yosys.py
+++ benchmarks/iccad-2022-prob-A/run_yosys.py
@@ -1,26 +1,52 @@
-
+from pathlib import Path
 import sys
 import os
 import subprocess
+from pathlib import Path
+import sys
+from cone import cone
 
-cmd = 'find . -name *.v'
+sys.path.append(str(Path(__file__).parent/'..'))
+import history
 
+cmd = 'find . -name \\*.v'
+cmd2 = 'find . -name \\*.sv'
 def run_cmd(cmd):
     a = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
     ax = a.stdout.decode('utf-8')
-    return ax
-#print('\n'.join(ax))
 
-ax = run_cmd(cmd).split()
+    return ax
+
+
+
+ax = run_cmd(cmd).split() + run_cmd(cmd2).split()
+#print('\n'.join(ax))
 for i in ax:
+    i = Path(i).name
+    print(i)
+    target_dir = 'test/%s' %  i[:i.rfind('.')]
+    print(target_dir)
+    os.system('mkdir -p %s' % target_dir)
+    
+    os.system(f'cp {i} {target_dir} ')
+    pwd = os.getcwd()
+    os.chdir (target_dir)
     e = 'yosys -p "read_verilog -sv %s ; hierarchy -auto-top ; flatten ; aigmap; write_aiger -symbols %s.aig"' % (i,i)
     print(e)
     os.system(e)
+    fname = '%s/%s.aig' % (target_dir, i)
+    os.chdir(pwd)
+
+    if os.path.exists(fname):
+        cone(fname)
+        pass
+
+
     pass
 
 
 def sort_them_by_size():
-    cmd ='ls -lhS */*/*.aig'
+    cmd ='ls -lhS *.aig'
     
     ax  = run_cmd(cmd).splitlines()
     ax = [i.split()[-1] for i in ax]
@@ -32,4 +58,4 @@ def sort_them_by_size():
         os.system(cmd)
     pass
 
-sort_them_by_size()
+#sort_them_by_size()
