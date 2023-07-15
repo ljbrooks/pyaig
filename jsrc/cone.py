d50231618d9c742692f08016daadfa24fddc5e28
Brooks LJ <jiang.long@gmail.com>
2023-07-15 22:07:49 +0800

    PROGRESS: run_yosys.py to synthesis .sv and .sv files and run cone.py to produce aig

--- jsrc/cone.py
+++ jsrc/cone.py
@@ -1,20 +1,31 @@
 import history
-
+from pathlib import Path
 import sys
-sys.path.append('..')
+
+sys.path.append(str(Path(__file__).parent/'..'))
 
 import pyaig
-aig, nx = sys.argv[1], list(map(int, sys.argv[2:]))
 
 
-print(aig, nx)
+def cone(aig):
+    
+    a = pyaig.aig_io.read_aiger(aig)
+    
+    if len(list(a.get_pos()))> 1:
+        nx = range(len(list(a.get_pos())))
+        for i in nx:
+            ox = [list(a.get_pos())[i]]
+            #ox = [j for i,j in enumerate(a.get_pos()) if i in nx]
+            
+            b = a.clean([i])
+            fname ='%s_out_%s.aig' % (aig, i)
+            pyaig.aig_io.write_aiger(b,fname)
+            print('Gen', fname)
+            pass
+        pass
+    pass
 
-a = pyaig.aig_io.read_aiger(aig)
-
-ox = [j for i,j in enumerate(a.get_pos()) if i in nx]
-
-b = a.clean(nx)
-fname ='%s_out_%s.aig' % (aig, '_'.join(map(str,nx)))
-pyaig.aig_io.write_aiger(b,fname)
-print('Gen', fname)
-
+if __name__ == '__main__':
+    
+    aig, nx = sys.argv[1], list(map(int, sys.argv[2:]))
+    cone(aig)
